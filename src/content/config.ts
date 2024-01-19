cdc3ef398a469be2904095db4277247df9995b9c
PiterDev <piterzdev@gmail.com>
2024-01-19 19:52:16 +0100

    Archivos sobrantes borrados

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,21 +2,7 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
-    category: z.string(),
-    tags: z.array(z.string()),
-  }),
-});
+
 
 const teamCollection = defineCollection({
   schema: z.object({
@@ -34,6 +20,5 @@ const teamCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
   'team': teamCollection,
 };
