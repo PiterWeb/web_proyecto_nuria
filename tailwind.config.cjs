b68cbd35ac577a188167f2834ddcfaa425a378c0
PiterDev <piterzdev@gmail.com>
2024-01-23 13:25:21 +0100

    Modo oscuro con posibilidad de activación y desactivación manual

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "dark-primary": "#e8e6e3",
+        "dark-secondary": "#8c8273",
+        "dark-bg": "#131516",
       },
     },
   },
